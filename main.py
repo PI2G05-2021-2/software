4f34ac312dd8ed5f8277ff1922899482f7da1644
Ugor Marcilio Brandão Costa <52638444+ubrando@users.noreply.github.com>
2022-04-04 15:06:33 -0300

    HotReload Implematation

--- main.py
+++ main.py
@@ -1,34 +1,48 @@
-from kivy.lang import Builder
+
+import os
+from kivy.core.window import Window
 from kivymd.app import MDApp
-from kivy.uix.screenmanager import Screen, ScreenManager
+from kaki.app import App
+from kivy.factory import Factory
+
+Window.size = (414, 736)
+
+# main app class for kaki app with kivymd modules
 
 
-class JanelaGerenciadora(ScreenManager):
-    pass
+class LiveApp(MDApp, App):
+    """ Hi Windows users """
 
+    DEBUG = 1  # set this to 0 make live app not working
 
-class TelaDePerfis(Screen):
-    pass
+    # *.kv files to watch
+    KV_FILES = {
+        os.path.join(os.getcwd(), "screens/mainscreenmanager.kv"),
+        os.path.join(os.getcwd(), "screens/screens_login/loginscreen.kv"),
+    }
 
+    # class to watch from *.py files
+    CLASSES = {
+        "MainScreenManager": "screens.mainscreenmanager",
+        "LoginScreen": "screens.screens_login.loginscreen",
+        "LaboratorioScreen": "screens.screens_laboratorio.laboratorioscreen",
+        "AcompanharScreen": "screens.screens_acompanhar.acompanharscreen",
+        "PerfilScreen": "screens.screens_perfil.perfilscreen",
 
-class PerfisdeExtracao(Screen):
-    pass
+    }
 
+    # auto reload path
+    AUTORELOADER_PATHS = [
+        (".", {"recursive": True}),
+    ]
 
-class InformacoesDePerfis(Screen):
-    pass
-
-
-class AcompanharExtracao(Screen):
-    pass
-
-
-class aplicativo(MDApp):
-    def build(self):
+    def build_app(self):
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Green"
         self.theme_cls.accent_palette = "Blue"
-        return Builder.load_file('aplicativo.kv')
+        return Factory.MainScreenManager()
 
 
-aplicativo().run()
+# finally, run the app
+if __name__ == "__main__":
+    LiveApp().run()
